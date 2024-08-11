57181d7a5c5a6ef6c111aa0527e95295a8e63b94
Wilmar Garcia <wilmar.garciava@gmail.com>
2024-08-11 16:11:59 -0500

    ✨ feat(roadmap): Implement interactive roadmap visualizer updatelogo

--- app/components/layout/Header.tsx
+++ app/components/layout/Header.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import {auth, currentUser} from "@clerk/nextjs/server";
 import {UserButton} from "@clerk/nextjs";
+import Image from "next/image";
 
 export async function Header() {
     const { userId } = auth();
@@ -9,21 +10,8 @@ export async function Header() {
     return (
         <header className="px-4 lg:px-6 h-14 flex items-center justify-between border-b">
             <Link href="/" className="flex items-center justify-center">
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className="h-6 w-6"
-                >
-                    <path d="m8 3 4 8 5-5 5 15H2L8 3z"></path>
-                </svg>
-                <span className="ml-2 text-lg font-semibold">Acme Software</span>
+                <Image src="/logo/logo_ucatolica.png" alt="Funlam logo" width={90} height={90} />
+                <span className="ml-2 text-lg font-semibold">Ucla Radmap App</span>
             </Link>
             <nav className="flex gap-4 sm:gap-6">
                 {['Roadmap', 'Features', 'Pricing', 'About'].map((item) => (
